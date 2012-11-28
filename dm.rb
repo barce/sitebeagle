deaf4c6c550b0ce93e438e4875c1f97c80fc3f3a
Barce <Barce@me.com>
2012-11-28 00:03:06 +0000

    adding dm.rb for sending dm

---
+++ dm.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/env ruby
+
+
+require "rubygems"
+require "date"
+require "yaml"
+require "net/http"
+require "uri"
+require "twitter_oauth"
+require 'digest/md5'
+require 'optparse'
+
+
+opts = OptionParser.new
+
+OptionParser.new do |o|
+  o.on('-h') { output_help; exit }
+  o.on('--user USER') { |user| $user = user }
+  o.on('--message MESSAGE') { |message| $message = message }
+  o.parse!
+end
+
+user    = $user
+message = $message
+
+yamlstring = ''
+File.open("./auth.yaml", "r") { |f|
+  yamlstring = f.read
+}
+
+settings = YAML::load(yamlstring)
+puts settings.inspect
+
+
+if settings['access_token'].nil?
+
+  puts "use get_oauth.rb to fill out info for access_token and access_secret."
+
+end
+
+
+client = TwitterOAuth::Client.new(
+	    :consumer_key => settings['consumer_key'],
+	    :consumer_secret => settings['consumer_secret'],
+	    :token => settings['access_token'],
+	    :secret => settings['access_secret']
+)
+
+
+
+
+
+
+puts "authorized? #{client.authorized?}"
+
+# url
+# alerts
+# send DMs to this account
+# regex
+
+client.message("#{user.chomp}", "#{message}")
