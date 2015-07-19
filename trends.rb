7104ff98f60f15a7de6f291d52befe06886e203e
Jim Barcelona <barce@radiumone.com>
2015-07-19 16:39:01 -0700

    making code DRY

--- trends.rb
+++ trends.rb
@@ -9,6 +9,7 @@ require "uri"
 require "twitter_oauth"
 require 'digest/md5'
 require 'optparse'
+require "./twitter_oauth_include.rb"
 
 
 opts = OptionParser.new
@@ -23,31 +24,8 @@ end
 user    = $user
 message = $message
 
-yamlstring = ''
-File.open("./auth.yaml", "r") { |f|
-  yamlstring = f.read
-}
 
-settings = YAML::load(yamlstring)
-puts settings.inspect
-
-
-if settings['access_token'].nil?
-
-  puts "use get_oauth.rb to fill out info for access_token and access_secret."
-
-end
-
-
-client = TwitterOAuth::Client.new(
-	    :consumer_key => settings['consumer_key'],
-	    :consumer_secret => settings['consumer_secret'],
-	    :token => settings['access_token'],
-	    :secret => settings['access_secret']
-)
-
-
-
+client = my_client
 
 
 
--- unfollower.rb
+++ unfollower.rb
@@ -10,6 +10,8 @@ require "uri"
 require "twitter_oauth"
 require 'digest/md5'
 require 'optparse'
+require "./twitter_oauth_include.rb"
+
 
 
 opts = OptionParser.new
@@ -20,30 +22,7 @@ OptionParser.new do |o|
 end
 
 
-yamlstring = ''
-File.open("./auth.yaml", "r") { |f|
-  yamlstring = f.read
-}
-
-settings = YAML::load(yamlstring)
-puts settings.inspect
-
-
-if settings['access_token'].nil?
-
-  puts "use get_oauth.rb to fill out info for access_token and access_secret."
-
-end
-
-
-client = TwitterOAuth::Client.new(
-	    :consumer_key => settings['consumer_key'],
-	    :consumer_secret => settings['consumer_secret'],
-	    :token => settings['access_token'],
-	    :secret => settings['access_secret']
-)
-
-
+client = my_client
 
 
 
