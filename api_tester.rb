da9046a0cd13fcf14ef42b3b3bb6cdd9fe353fde
Jim Barcelona <barce@radiumone.com>
2015-07-19 16:37:54 -0700

    use twitter_oauth_include.rb

--- api_tester.rb
+++ api_tester.rb
@@ -8,35 +8,15 @@ require "net/http"
 require "uri"
 require "twitter_oauth"
 require 'digest/md5'
+require "./twitter_oauth_include.rb"
+
 
 
 
     # s_data = Net::HTTP.get_response(URI.parse(@url)).body
     # s_md5  = Digest::MD5.hexdigest(s_data)
 
-yamlstring = ''
-File.open("./auth.yaml", "r") { |f|
-  yamlstring = f.read
-}
-
-settings = YAML::load(yamlstring)
-puts settings.inspect
-
-
-if settings['access_token'].nil?
-
-  puts "use get_oauth.rb to fill out info for access_token and access_secret."
-
-end
-
-
-client = TwitterOAuth::Client.new(
-	    :consumer_key => settings['consumer_key'],
-	    :consumer_secret => settings['consumer_secret'],
-	    :token => settings['access_token'],
-	    :secret => settings['access_secret']
-)
-
+client = my_client
 
 
 
